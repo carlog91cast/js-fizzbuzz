3e6117e774c2bb0ae26f3b24ebcca13018835b6f
bigeppo <carlog91cast@gmail.com>
2022-06-16 14:47:03 +0200

    add for (MILESTONE 1)

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Scrivi un programma che stampi in console i numeri da 1 a 100,
+// ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+// MILESTONE 1
+
+// per il p.to 1 creo un for (ciclo) che mi permetta di stampare in console i numeri da 1 a 100
+// inizializzo una variabile a un ponendo index <= 100 cosi da ricomprendere i valori da 1 a 100 compreso; tutto quanto scritto nel primo blocco verrà ripetuto 100 volte fino all'esaurimento di quanto posto in condizione
+
+for (let index = 1; index <= 100; index++) {
+    console.log(index)
+
+
+}
