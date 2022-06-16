480cfc2307287cb5773a7eca90a60072c4d638c1
bigeppo <carlog91cast@gmail.com>
2022-06-16 17:09:54 +0200

    add else with correct order on if

--- js/main.js
+++ js/main.js
@@ -7,17 +7,21 @@
 // per il p.to 1 creo un for (ciclo) che mi permetta di stampare in console i numeri da 1 a 100
 // inizializzo una variabile a un ponendo index <= 100 cosi da ricomprendere i valori da 1 a 100 compreso; tutto quanto scritto nel primo blocco verrà ripetuto 100 volte fino all'esaurimento di quanto posto in condizione
 
+
 for (let i = 1; i <= 100; i++) {
     console.log(i);
     // determino con un operatore ogniqualvolta i sia multiplo di 3
-    if (i % 3 == 0) {
-        console.log('Fizz');
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log('FizzBuzz');
         // ...ogniqualvolta i sia multiplo di 5
+    } else if (i % 3 == 0) {
+        console.log('Fizz');
+        // ogniqualvolta i sia multiplo sia di 3 che di 5
     } else if (i % 5 == 0) {
         console.log('Buzz');
-        // ogniqualvolta i sia multiplo sia di 3 che di 5
-    } else if (i % 3 && i % 5) {
-        console.log('FizzBuzz')
+    } else {
+        console.log('index')
     }
+
 
 }
