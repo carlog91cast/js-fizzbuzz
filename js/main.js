2ed0898c9717617012bdf84ea85f022f183b197a
bigeppo <carlog91cast@gmail.com>
2022-06-16 15:04:12 +0200

    add if (3 multiplies)

--- js/main.js
+++ js/main.js
@@ -7,8 +7,12 @@
 // per il p.to 1 creo un for (ciclo) che mi permetta di stampare in console i numeri da 1 a 100
 // inizializzo una variabile a un ponendo index <= 100 cosi da ricomprendere i valori da 1 a 100 compreso; tutto quanto scritto nel primo blocco verrà ripetuto 100 volte fino all'esaurimento di quanto posto in condizione
 
-for (let index = 1; index <= 100; index++) {
-    console.log(index)
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+    if (i % 3) {
+        console.log('è multiplo di 3')
+        
+    }
 
 
 }
